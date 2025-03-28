596f59d4c7f3f49d5c8264f8e2ae13f71855de69
Florin Mihai Ciuperciuc <flomihciu@gmail.com>
2025-03-28 11:24:56 -0500

    Troubleshooting

--- terraform/s3-backend.tf
+++ terraform/s3-backend.tf
@@ -1,30 +1,4 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "do-assessment3-movie-db-flo"
+# S3 and DynamoDB backend resources were moved out of Terraform-managed apply
+# They are now created conditionally in the GitHub Actions workflow if needed.
 
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-lock-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform Lock Table"
-  }
-}
+# This file is intentionally left empty.
