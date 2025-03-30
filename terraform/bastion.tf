8a89c693760aa681a794dbcaea5a7bfa32a2268b
Florin Mihai Ciuperciuc <flomihciu@gmail.com>
2025-03-30 18:13:11 -0500

    Troubleshooting

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "bastion_sg" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                         = "ami-0c02fb55956c7d316" # ✅ Ubuntu 22.04 LTS (us-east-1)
+  ami                         = "ami-084568db4383264d4" 
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public_subnet_1.id
   associate_public_ip_address = true
