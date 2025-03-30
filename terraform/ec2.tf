76565577094134acbc8f0e93187ed683aca136c9
Florin Mihai Ciuperciuc <flomihciu@gmail.com>
2025-03-30 13:55:22 -0500

    Troubleshooting

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "app_server" {
   instance_type               = var.ec2_instance_type
   key_name                    = var.ec2_key_pair_name
   subnet_id                   = aws_subnet.public_subnet_1.id
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
